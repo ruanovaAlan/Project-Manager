85f29e4a4e82269b0f351658ce75555ca1a2d17e
Alan Ruanova <ruanovaalan96@gmail.com>
2024-01-18 21:45:48 -0600

    Create ProjectsSideBar component

---
+++ src/components/ProjectsSideBar.jsx
@@ -0,0 +1,9 @@
+export default function ProjectsSideBar() {
+    return <aside>
+        <h2>Your Projects</h2>
+        <div>
+            <button>+ Add Project</button>
+        </div>
+        <ul></ul>
+    </aside>
+}
