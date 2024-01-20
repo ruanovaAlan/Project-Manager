d6677d798137ca4b1319d3c10928c0e79177beb9
Alan Ruanova <ruanovaalan96@gmail.com>
2024-01-19 20:29:44 -0600

    Add selectedProjectId to state and pass projects to ProjectsSideBar component

--- src/App.jsx
+++ src/App.jsx
@@ -27,6 +27,7 @@ function App() {
       }
       return {
         ...prevState,
+        selectedProjectId: undefined,
         projects: [...prevState.projects, newProject]
       }
     })
@@ -42,7 +43,7 @@ function App() {
 
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSideBar onStartAddProject={handleStartAddProject} />
+      <ProjectsSideBar onStartAddProject={handleStartAddProject} projects={projectsState.projects} />
       {content}
     </main>
   );
